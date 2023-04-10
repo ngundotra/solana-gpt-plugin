f2a374fab4796bd724879681e70b4062453f6fc5
ngundotra <noah@gundotra.org>
2023-04-10 16:40:34 -0400

    clean up code with some comments

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,9 @@ import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
 const app = express();
 const port = process.env.PORT || 3333;
 
-const solanaRPCUrl = "https://api.mainnet-beta.solana.com";
-const connection = new Connection(solanaRPCUrl);
+const HELIUS_URL = `https://rpc.helius.xyz/?api-key=${process.env.HELIUS_API_KEY}}`;
+const SOLANA_RPC_URL = "https://api.mainnet-beta.solana.com";
+const connection = new Connection(SOLANA_RPC_URL);
 
 app.use(bodyParser.json());
 app.use(
@@ -42,9 +43,13 @@ function replaceBNWithToString(obj: any): any {
   return obj;
 }
 
-console.log(process.env.HELIUS_API_KEY);
-const HELIUS_URL = `https://rpc.helius.xyz/?api-key=${process.env.HELIUS_API_KEY}}`;
-
+/**
+ * Returns the data from the Metaplex Read API
+ * @param address
+ * @param page (optional) page number
+ * @param limit (optional) set to 5 to prevent overflowing GPT context window
+ * @returns
+ */
 const getAssetsByOwner = async (
   address: string,
   page: number = 1,
@@ -65,59 +70,64 @@ const getAssetsByOwner = async (
   return data.result;
 };
 
+/**
+ * Returns accountInfo or extends it with deserialized account data if the account is a program account of an Anchor program
+ * @param accountAddress
+ * @returns
+ */
+async function getAccountInfo(accountAddress: PublicKey): Promise<Object> {
+  const accountInfo = await connection.getAccountInfo(accountAddress);
+  if (accountInfo?.owner && !accountInfo.executable) {
+    try {
+      const program = await Program.at(
+        accountInfo.owner,
+        new AnchorProvider(connection, new NodeWallet(Keypair.generate()), {
+          commitment: "confirmed",
+        })
+      );
+
+      const rawData = accountInfo.data;
+      const coder = new BorshAccountsCoder(program.idl);
+      const accountDefTmp = program.idl.accounts?.find((accountType: any) =>
+        (rawData as Buffer)
+          .slice(0, 8)
+          .equals(BorshAccountsCoder.accountDiscriminator(accountType.name))
+      );
+
+      if (accountDefTmp) {
+        const accountDef = accountDefTmp;
+        const decodedAccountData = replaceBNWithToString(
+          coder.decode(accountDef.name, rawData)
+        );
+        console.log(decodedAccountData);
+
+        let payload = {
+          ...accountInfo,
+          extended: JSON.stringify(decodedAccountData),
+        };
+        return payload;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return accountInfo || {};
+}
+
 app.post("/:methodName", async (req, res) => {
+  // Inspect what ChatGPT is sending
   console.log(req.params.methodName, req.body);
+
+  // Dispatch the request
   try {
     if (req.params.methodName === "getAssetsByOwner") {
       const accountAddress = new PublicKey(req.body.address);
       const assets = await getAssetsByOwner(accountAddress.toString());
       res.status(200).send({ message: JSON.stringify(assets) });
-    }
-    if (req.params.methodName === "getAccountInfo") {
+    } else if (req.params.methodName === "getAccountInfo") {
       const accountAddress = new PublicKey(req.body.address);
-      const accountInfo = await connection.getAccountInfo(accountAddress);
-      if (accountInfo?.owner && !accountInfo.executable) {
-        try {
-          const program = await Program.at(
-            accountInfo.owner,
-            new AnchorProvider(connection, new NodeWallet(Keypair.generate()), {
-              commitment: "confirmed",
-            })
-          );
-
-          const rawData = accountInfo.data;
-          const coder = new BorshAccountsCoder(program.idl);
-          const accountDefTmp = program.idl.accounts?.find((accountType: any) =>
-            (rawData as Buffer)
-              .slice(0, 8)
-              .equals(BorshAccountsCoder.accountDiscriminator(accountType.name))
-          );
-          if (accountDefTmp) {
-            const accountDef = accountDefTmp;
-            try {
-              const decodedAccountData = replaceBNWithToString(
-                coder.decode(accountDef.name, rawData)
-              );
-              console.log(decodedAccountData);
-
-              let payload = {
-                ...accountInfo,
-                extended: JSON.stringify(decodedAccountData),
-              };
-              res.status(200).send({ message: JSON.stringify(payload) });
-              return;
-            } catch (err) {
-              console.log(err);
-              res.status(500).send({ message: "An error occurred" });
-              return;
-            }
-          }
-        } catch (err) {
-          res.status(200).send({ message: JSON.stringify(accountInfo) });
-        }
-      } else {
-        res.status(200).send({ message: JSON.stringify(accountInfo) });
-      }
+      const accountInfo = await getAccountInfo(accountAddress);
+      res.status(200).send({ message: JSON.stringify(accountInfo) });
     } else if (req.params.methodName === "getBalance") {
       const { address } = req.body;
       const balance = await connection.getBalance(new PublicKey(address));
@@ -125,6 +135,8 @@ app.post("/:methodName", async (req, res) => {
     }
   } catch (error) {
     console.error(error);
+
+    // Prevent ChatGPT from getting access to error messages until we have a better error handling
     res.status(500).send({ message: "An error occurred" });
   }
 });
